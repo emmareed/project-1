ee7689d65c76b6d94c1d533552baf2a23134eff4
Emma Reed <ereed5@uoregon.edu>
2020-04-09 21:20:28 -0700

    Add Learning Objectives to README.md

--- README.md
+++ README.md
@@ -5,3 +5,7 @@
 - Basic Unix Shell commands
 - Install and Configure Git using the Unix Shell
 - Github overview
+- Basic Git Commands (add, commit, push, clone)
+- Markdown language for README files
+- JavaScript ES6+ features
+- Using Terminal 
---
+++ javascripts/functions.js
@@ -0,0 +1,28 @@
+// jshint esversion: 7
+
+//Req. 3b
+let arrAvg = (arr) => {
+  let sum = 0;
+
+  for (var i = 0; i < arr.length; i++) {
+   //arr[i]
+  }
+};
+
+//Req. 3c
+let arrMax = (arr) ==> {
+  //
+};
+//=======================================
+let fruits = ["Apple", "Orange", "Plum"];
+let maxSoFar = fruits[0].length;
+
+//iterate over array ay
+fruits.forEach((elem) => {
+  if (elem.length > maxSoFar) {
+    maxSoFar = elem.length;
+  }
+  result = `${result} ${elem} `;
+});
+
+console.log(maxSoFar);
