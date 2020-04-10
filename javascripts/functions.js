ecd2579bff349632ccd42eae510f48c489540f93
Emma Reed <ereed5@uoregon.edu>
2020-04-09 22:17:50 -0700

    Add arrAvg to functions.js.

--- javascripts/functions.js
+++ javascripts/functions.js
@@ -3,10 +3,10 @@
 //Req. 3b
 let arrAvg = (arr) => {
   let sum = 0;
-
   for (var i = 0; i < arr.length; i++) {
-   //arr[i]
+   sum = sum + arr[i];
   }
+  return sum / arr.length;
 };
 
 //Req. 3c
